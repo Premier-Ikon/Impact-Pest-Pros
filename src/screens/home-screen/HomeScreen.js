03be0f66018774ea627ebd47f6bee2424373b879
Dylan Guzman <dylanaguzman@gmail.com>
2022-08-24 20:16:43 -0700

    updated home page

--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -7,6 +7,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { Bar } from 'react-chartjs-2'
 
 import {Button} from '../../components/Buttons'
 import {ImageElements} from "../../components/ImageElements/ImageElements";
@@ -26,6 +27,7 @@ import SpiderImage from '../../assets/SPIDER-ICON.png'
 import RodentImage from '../../assets/RODENT-ICON.png'
 import JoinUs from "../../icons/JOIN_US.png";
 import {Link} from "react-router-dom";
+import AdLogo from "../../assets/AD-LOGO.png";
 
 
 function HomeScreen() {
@@ -119,6 +121,53 @@ function HomeScreen() {
                 </div>
             </div>
 
+            <div className="benefits-container">
+
+                <div className="general-header-white">
+                    <h2 className="header-text-side">Why You Need Pest Control</h2>
+                </div>
+                <div className="align-center">
+                    <div className="horizontal-line"/>
+                </div>
+                <div className="benefits-elements">
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={CustomerSupport} description={"Protect Your Family"} style={"white"}/>
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={PaymentProcess} description={"Protect Your Pets"} style={"white"}/>
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={Integrity} description={"Protect Your Home"} style={"white"}/>
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={Scheduling} description={"Pest Free Home"} style={"white"}/>
+                </div>
+                {/*<div className={'button-container-pest-control'}>*/}
+                {/*    <Link to={"/contact-us"}>*/}
+                {/*        <button className={'pest-control-button-style'}>*/}
+                {/*            <p className="service-button-text">Get Started Today</p>*/}
+                {/*        </button>*/}
+                {/*    </Link>*/}
+                {/*</div>*/}
+            </div>
+            <div className="benefits-container-mobile">
+                <div className="white-header-mobile">
+                    <h2 className="pest-control-header">Why You Need Pest Control</h2>
+                </div>
+                <div className="align-center">
+                    <div className="horizontal-line"/>
+                </div>
+                <div className="benefits-elements-mobile">
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={CustomerSupport} description={"Protect Your Family"} style={"white"}/>
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={PaymentProcess} description={"Protect Your Pets"} style={"white"}/>
+                </div>
+                <div className="benefits-elements-mobile">
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={Integrity} description={"Protect Your Home"} style={"white"}/>
+                    {/* eslint-disable-next-line react/style-prop-object */}
+                    <ImageElements image={Scheduling} description={"Pest Free Home"} style={"white"}/>
+                </div>
+            </div>
+
             <div className="rodent-control-container">
                 <div className="general-header">
                     <h2>How Our Services Work</h2>
@@ -177,38 +226,70 @@ function HomeScreen() {
             </div>
 
             <div className="benefits-container">
-                <div className="margining-side">
-                    <div className="side-header">
-                        <h2 className="header-text-side">Benefits</h2>
-                    </div>
-                    <div className="line"/>
+
+                <div className="general-header-white">
+                    <h2 className="header-text-side">Why Choose Impact Pest Pros</h2>
+                </div>
+                <div className="align-center">
+                    <div className="horizontal-line"/>
                 </div>
                 <div className="benefits-elements">
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={CustomerSupport} description={"Customer Service"} style={"white"}/>
+                    <ImageElements image={CustomerSupport} description={"Family Owned"} style={"white"}/>
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={PaymentProcess} description={"Payment Process"} style={"white"}/>
+                    <ImageElements image={PaymentProcess} description={"Company Integrity"} style={"white"}/>
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={Integrity} description={"Company Integrity"} style={"white"}/>
+                    <ImageElements image={Integrity} description={"Customer Service"} style={"white"}/>
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={Scheduling} description={"On-Time Services"} style={"white"}/>
+                    <ImageElements image={Scheduling} description={"On Time Services"} style={"white"}/>
                 </div>
+                {/*<div className={'button-container-pest-control'}>*/}
+                {/*    <Link to={"/contact-us"}>*/}
+                {/*        <button className={'pest-control-button-style'}>*/}
+                {/*            <p className="service-button-text">Get Started Today</p>*/}
+                {/*        </button>*/}
+                {/*    </Link>*/}
+                {/*</div>*/}
             </div>
             <div className="benefits-container-mobile">
                 <div className="white-header-mobile">
-                    <h2>Benefits</h2>
+                    <h2 className="pest-control-header">Why Choose Impact Pest Pros</h2>
+                </div>
+                <div className="align-center">
+                    <div className="horizontal-line"/>
                 </div>
                 <div className="benefits-elements-mobile">
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={CustomerSupport} description={"Customer Support"} style={"white"}/>
+                    <ImageElements image={CustomerSupport} description={"Family Owned"} style={"white"}/>
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={PaymentProcess} description={"Payment Process"} style={"white"}/>
+                    <ImageElements image={PaymentProcess} description={"Company Integrity"} style={"white"}/>
                 </div>
                 <div className="benefits-elements-mobile">
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={Integrity} description={"Company Integrity"} style={"white"}/>
+                    <ImageElements image={Integrity} description={"Customer Service"} style={"white"}/>
                     {/* eslint-disable-next-line react/style-prop-object */}
-                    <ImageElements image={Scheduling} description={"Scheduling"} style={"white"}/>
+                    <ImageElements image={Scheduling} description={"On Time Services"} style={"white"}/>
+                </div>
+            </div>
+
+            <div className="internal-ad">
+                <div className="image-container">
+                    <div className="logo-container">
+                        <img src={AdLogo} className="ad-logo" alt={"ad-logo"}/>
+                    </div>
+                    <div className="vertical-line"/>
+                    <div className="lower-container">
+                        <div className="service-ad-text">
+                            <p className="test">Insects are swarming nationwide and your home needs protection. Luckily, our pest protection plans start as low as $23/mo. Click below to get your FREE quote and ensure your home is protected from pests in order to make your home happy and clean.</p>
+                        </div>
+                        <div className="internal-ad-button">
+                            <Link to={"/contact-us"}>
+                                <button className="service-ad-button">
+                                    <p>Free Quote</p>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -227,13 +308,39 @@ function HomeScreen() {
                     onSlideChange={() => console.log('slide change')}
                     className={"swiper-element-review"}
                 >
-                    <SwiperSlide className={"test-test"}>
-                        <CustomerReview name={"Patty Esparza"} review={'"We contacted Impact Pest Pros for there services and I talked to the Business owner Andres. He was very honest and open about our immediate problem. He came to our home and took care of our issue. He answered all of our questions and was very knowledgeable! I would highly recommend him with any pest problem in your home."'}/>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <CustomerReview name={"McKayla Guzman"} review={'"They provide great service with clean and safe products. Having safe products was a big thing I wanted at my home because I have pets. Since having impact pest pros service my house the site of bugs and insects and greatly decreased. They spend good time at my home so they can make sure everything is done correctly. They are very accommodating if I have a problem they will do their best to get out as quick as possible."'}/>
-                    </SwiperSlide>
+                    <div className="test-test">
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Patty E."} review={'"We contacted Impact Pest Pros for there services and I talked to the Business owner Andres. He was very honest and open about our immediate problem. He came to our home and took care of our issue. He answered all of our questions and was very knowledgeable! I would highly recommend him with any pest problem in your home."'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"McKayla G."} review={'"They provide great service with clean and safe products. Having safe products was a big thing I wanted at my home because I have pets. Since having impact pest pros service my house the site of bugs and insects and greatly decreased. They spend good time at my home so they can make sure everything is done correctly. They are very accommodating if I have a problem they will do their best to get out as quick as possible."'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Ferney E."} review={'"We have been very pleased with Impact Pest Pros, ever since we\'ve used them we no longer have random insects coming inside .We are on 5 acres of land and have plenty of wild life. They are timely and professional, I will continue with there service .\n' +
+                                '\n' +
+                                'Andres is very knowledgeable and is very thorough with his work."'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Jessica G."} review={'"Andres does a wonderful job at keeping the pests away. We had a ton of spiders and a bird problem on our solar panels and he took care of both and I couldn\'t be happier."'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Felipe M."} review={'"We have had trouble with ants.Andres came to the rescue to help with the ants.Now we have them under control. Thanks Impact pest control."'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Rob C."} review={'"Andres and Impact Pest Pros are fantastic. I use Andres for two of our properties, Including an Airbnb. He\'s gone above and beyond to make sure that everything is perfect. I\'ve gone through quite a few pest control companies, and Andres is the best. My highest recommendation!"'}/>
+                        </SwiperSlide>
+                        <SwiperSlide className="test-test">
+                            <CustomerReview name={"Michele R."} review={'"If your looking for a great pest control company this is your guy!  I\'ve been using his services for over a year now and have been completely satisfied!  I would recommend this company to anyone!"'}/>
+                        </SwiperSlide>
+                    </div>
                 </Swiper>
+                <div className={'button-container-service'}>
+                    <Link to={"/contact-us"}>
+                        <button className={'service-button-style'}>
+                            <p className="service-button-text">Join Today</p>
+                        </button>
+                    </Link>
+                </div>
             </div>
             <div className="review-container-mobile">
                 <div className="general-header">
@@ -250,17 +357,35 @@ function HomeScreen() {
                     onSlideChange={() => console.log('slide change')}
                     className={"swiper-element-review"}
                 >
-                    <SwiperSlide className={"test-test"}>
-                        <CustomerReview name={"Patty Esparza"} review={'"We contacted Impact Pest Pros for there services and I talked to the Business owner Andres. He was very honest and open about our immediate problem. He came to our home and took care of our issue. He answered all of our questions and was very knowledgeable! I would highly recommend him with any pest problem in your home."'}/>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"McKayla G."} review={'"They provide great service with clean and safe products. Having safe products was a big thing I wanted at my home because I have pets. Since having impact pest pros service my house the site of bugs and insects and greatly decreased. They spend good time at my home so they can make sure everything is done correctly. They are very accommodating if I have a problem they will do their best to get out as quick as possible."'}/>
                     </SwiperSlide>
-                    <SwiperSlide>
-                        <CustomerReview name={"McKayla Guzman"} review={'"They provide great service with clean and safe products. Having safe products was a big thing I wanted at my home because I have pets. Since having impact pest pros service my house the site of bugs and insects and greatly decreased. They spend good time at my home so they can make sure everything is done correctly. They are very accommodating if I have a problem they will do their best to get out as quick as possible."'}/>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Patty E."} review={'"We contacted Impact Pest Pros for there services and I talked to the Business owner Andres. He was very honest and open about our immediate problem. He came to our home and took care of our issue. He answered all of our questions and was very knowledgeable! I would highly recommend him with any pest problem in your home."'}/>
                     </SwiperSlide>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Ferney E."} review={'"We have been very pleased with Impact Pest Pros, ever since we\'ve used them we no longer have random insects coming inside .We are on 5 acres of land and have plenty of wild life. They are timely and professional, I will continue with there service .\n' +
+                            '\n' +
+                            'Andres is very knowledgeable and is very thorough with his work."'}/>
+                    </SwiperSlide>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Jessica G."} review={'"Andres does a wonderful job at keeping the pests away. We had a ton of spiders and a bird problem on our solar panels and he took care of both and I couldn\'t be happier."'}/>
+                    </SwiperSlide>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Felipe M."} review={'"We have had trouble with ants.Andres came to the rescue to help with the ants.Now we have them under control. Thanks Impact pest control."'}/>
+                    </SwiperSlide>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Rob C."} review={'"Andres and Impact Pest Pros are fantastic. I use Andres for two of our properties, Including an Airbnb. He\'s gone above and beyond to make sure that everything is perfect. I\'ve gone through quite a few pest control companies, and Andres is the best. My highest recommendation!"'}/>
+                    </SwiperSlide>
+                    <SwiperSlide className="test-test">
+                        <CustomerReview name={"Michele R."} review={'"If your looking for a great pest control company this is your guy!  I\'ve been using his services for over a year now and have been completely satisfied!  I would recommend this company to anyone!"'}/>
+                    </SwiperSlide>
+
                 </Swiper>
             </div>
 
             <div className="quote-container">
-                <div className="general-header">
+                <div className="general-header-white">
                     <h2>Get a FREE Quote Today</h2>
                 </div>
                 <div className="quote-button">
